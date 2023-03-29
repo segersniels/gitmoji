0ccaa06a02fea28b5fe8fb3a6109ff1c40a41a3d
segersniels <segers.n@hotmail.com>
2023-03-29 14:06:15 +0200

    :children_crossing: Improve prompt experience

--- src/helpers/OpenAI.ts
+++ src/helpers/OpenAI.ts
@@ -19,6 +19,8 @@ export async function generatePrompt(diff: string, gitmojis: Gitmoji[]) {
     Are they fixing a bug, adding a new feature, improving performance or readability, or something else?
     Use this information to craft a concise and detailed gitmoji commit message that clearly describes what the provided code or diff does.
 
+    Describe the change to the best of your capabilities in one short sentence. Don't go into too much detail.
+
     When reviewing a diff, pay attention to the changed filenames and extract the context of the changes.
     This will help you create a more relevant and informative commit message.
     Here are some examples of how you can interpret some changed filenames:
@@ -31,9 +33,11 @@ export async function generatePrompt(diff: string, gitmojis: Gitmoji[]) {
     """
 
     Try to match the generated message to a fitting emoji using its description from the provided list above.
+    So go look in the descriptions and find the one that best matches the description.
+    So a change related to a refactor would best match with ":recycle: - Refactor code." from the list and would choose :recycle: as the gitmoji.
+
     Always start your commit message with a gitmoji followed by the message starting with a capital letter.
-    Limit yourself to one sentence but don't end it in a punctuation mark and try to stay within 80 characters.
-    Never mention file names or function names in the message.
+    Never mention filenames or function names in the message.
 
     Don't do this:
       - :bug: Fix issue in calculateTotalPrice function
